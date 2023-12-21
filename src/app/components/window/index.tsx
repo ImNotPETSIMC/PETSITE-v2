89e581e5a9851cb22b6650ee40f7dc513efa78ec
Enzo <enzowederma@gmail.com>
2023-12-20 21:19:40 -0300

    🐞 fix: change buttons align on window component

--- src/app/components/window/index.tsx
+++ src/app/components/window/index.tsx
@@ -5,7 +5,7 @@ export const Window = () => {
     return (
         <>
             <div className="border-w95-grey border-2 border-solid w-fit">
-                <div className="border-w95-grey border-b-2 border-solid w-full h-6 bg-[#593CFA] flex items-center gap-2 pl-10 pr-1">
+                <div className="border-w95-grey border-b-2 border-solid w-full h-6 bg-[#593CFA] flex items-center justify-end gap-2 pl-10 pr-1">
                     <div className="flex items-center bg-w95-grey h-fit w-fit p-0.5">
                         <div className="border-b border-solid border-black h-3 w-3"></div>
                     </div>
@@ -16,7 +16,9 @@ export const Window = () => {
                         <img src="/images/x_icon.svg" alt="X" />
                     </div>
                 </div>
-                <div className="bg-white">-</div>
+                <div className="bg-white">
+                    content
+                </div>
             </div>
         </>
     )
