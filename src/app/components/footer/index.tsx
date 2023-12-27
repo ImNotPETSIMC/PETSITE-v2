a3642c4f682fefdb69f5bd98d2b0fd217c1a25b8
Enzo <enzowederma@gmail.com>
2023-12-27 18:07:30 -0300

    🦄 refactor: clock tweaks and anchor elements rebuild
    Removes div to use anchor directly on footer.
    
    Tweak clock logic to avoid hydration errors and keep it updated.

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -4,20 +4,24 @@ import { useEffect, useState } from "react";
 export const Footer = () => {
     const [today, setToday] = useState<Date>(new Date());
 
-    useEffect(() => {setToday(new Date())})
+    useEffect(() => {
+        const timeInterval = setInterval(() =>  {
+            setToday(new Date())
+        }, 1000);
+    
+        return () => clearInterval(timeInterval);
+    }, [])
 
     return (
-        <div className="w-full h-14 bg-w95-darker-grey flex items-center px-2 font-ps2p mt-32 justify-between">
-            <div className="flex gap-2 items-center">
-                <a href="#">
-                    <div className="h-10 flex items-center gap-2 bg-w95-grey border-w95-dark-grey border-4 border-solid text-xs px-2">
-                        <Image 
-                            src= "/images/home_icon.png"
-                            alt="Home Icon"
-                            width={24}
-                            height={24}/>
-                        Start
-                    </div>
+        <div className="h-14 bg-w95-darker-grey flex items-center px-2 font-ps2p mt-32 justify-between">
+            <div className="flex gap-2 items-center min-w-fit">
+                <a href="#" className="h-10 flex items-center gap-2 bg-w95-grey border-w95-dark-grey border-4 border-solid text-xs px-2">
+                    <Image 
+                        src= "/images/home_icon.png"
+                        alt="Home Icon"
+                        width={24}
+                        height={24}/>
+                    Start
                 </a>
                 <div className="h-12 min-w-40 flex items-center gap-2 bg-w95-grey border-w95-dark-grey border-4 border-solid text-xs px-2">
                     <Image 
@@ -27,32 +31,28 @@ export const Footer = () => {
                         height={24}/>
                     PET-SIMC
                 </div>
-                <a href="https://ufu.br">
-                    <div className="h-12 min-w-40 flex items-center gap-2 bg-w95-grey border-w95-dark-grey border-4 border-solid text-xs px-2">
-                        <Image 
-                            src= "/images/ufu_logo.png"
-                            alt="UFU Logo"
-                            width={24}
-                            height={24}/>
-                        UFU
-                    </div>
+                <a href="https://ufu.br" className="h-12 min-w-40 flex items-center gap-2 bg-w95-grey border-w95-dark-grey border-4 border-solid text-xs px-2">
+                    <Image 
+                        src= "/images/ufu_logo.png"
+                        alt="UFU Logo"
+                        width={24}
+                        height={24}/>
+                    UFU
                 </a>
-                <a href="https://facom.ufu.br">
-                    <div className="h-12 min-w-40 flex items-center gap-2 bg-w95-grey border-w95-dark-grey border-4 border-solid text-xs px-2">
+                <a href="https://facom.ufu.br" className="h-12 min-w-40 flex items-center gap-2 bg-w95-grey border-w95-dark-grey border-4 border-solid text-xs px-2">
                         <Image 
                             src= "/images/facom_logo.png"
                             alt="FACOM Logo"
                             width={24}
                             height={24}/>
                         FACOM
-                    </div>
+
                 </a>
             </div>
-            <div className="h-10 flex items-center gap-2 bg-w95-grey border-w95-dark-grey border-4 border-solid text-xs px-2">
-                {today.getHours() + ":" + today.getMinutes()}
+            <div className="h-10 min-w-20 flex items-center gap-2 bg-w95-grey border-w95-dark-grey border-4 border-solid text-xs px-2">
+                {today.getHours() + ":" + (today.getMinutes().toString().length == 1 ? 0 : "") + today.getMinutes()}
             </div>  
-        </div>
-        
+        </div> 
     )
 
 }
