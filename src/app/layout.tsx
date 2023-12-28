72fb5b53d5fe3785a404006a1f6172cf256a4a9f
Enzo <enzowederma@gmail.com>
2023-12-28 11:21:46 -0300

    🌈 style: tweak on mobile height fit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,21 @@
 import type { Metadata } from 'next'
+import { Viewport } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
   title: 'PET-SIMC - Website',
-  description: "PETSIMC's Website to get to know projects, members, about and more...",
+  description: "PETSIMC's Website to get to know projects, members, about and more..."
+}
+
+export const viewport: Viewport = { 
+  initialScale: 0
 }
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
-}) {
+}) {  
   return (
     <html lang="en">
       <body>{children}</body>
