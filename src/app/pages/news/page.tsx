652e1a266cfa9cf8600aba67f4d85122e6fd94a5
Enzo <enzowederma@gmail.com>
2023-12-28 21:19:48 -0300

    🐞 fix: class not applied to window break
    Class condition apply wasn't working because of missing parenthesis

--- src/app/pages/news/page.tsx
+++ src/app/pages/news/page.tsx
@@ -96,7 +96,7 @@ const News = () => {
                     <div className='z-10 translate-x-2 2xl:translate-x-20 flex basis-full'>
                       <NewsContainer news={element} />
                     </div>
-                    <div className={ 'basis-full' + index % 2 ? ' flex justify-end max-w-7xl' : ' -translate-x-40'}>
+                    <div className={ 'basis-full' + (index % 2 ? ' flex justify-end max-w-7xl' : ' -translate-x-40')}>
                       <Window>
                         <div className='font-ps2p text-lg py-4 px-8'>
                           <p className='text-w95-blue flex'>printf<span className='text-[#009ED0]'>(<span className='text-black'>"hello world"</span>)</span></p>
