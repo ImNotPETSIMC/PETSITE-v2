54d114cb4199044c99bdd1b2df82628dc5ecddd7
Enzo <enzowederma@gmail.com>
2023-12-30 17:54:52 -0300

    ✨ feat: functional buttons on windows

--- src/app/components/window/index.tsx
+++ src/app/components/window/index.tsx
@@ -1,27 +1,35 @@
 import Image from "next/image"
+import { MouseEventHandler, useEffect, useState } from "react"
 
-export const Window = ({ children }: { children: React.ReactNode }) => {
+type WindowProps = {
+    children: React.ReactNode
+    onclose?: MouseEventHandler<HTMLButtonElement>
+}
+
+export const Window = (props: WindowProps) => {
+    const [closed, setClosed] = useState<boolean>(false);
+    const [minimized, setMinimized] = useState<boolean>(false);
 
     return (
-        <div className="border-w95-grey border-8 border-solid w-fit h-fit">
+        <div className={"border-w95-grey border-8 border-solid w-fit h-fit transition-all duration-500 " + (closed ? "opacity-0 -z-50" : "")}>
             <div className="border-w95-grey border-b-2 border-solid w-full h-6 bg-w95-blue flex items-center justify-end gap-2 pl-10 pr-1">
-                <div className="flex items-center bg-w95-grey h-fit w-fit p-0.5">
+                <button className="flex items-center bg-w95-grey hover:bg-w95-dark-grey h-fit w-fit p-0.5" onClick={() => setMinimized(true)}>
                     <div className="border-b border-solid border-black h-3 w-3"></div>
-                </div>
-                <div className="flex items-center bg-w95-grey h-fit w-fit p-0.5">
+                </button>
+                <button className="flex items-center bg-w95-grey hover:bg-w95-dark-grey h-fit w-fit p-0.5" onClick={() => setMinimized(false)}>
                     <div className="border border-solid border-black h-3 w-3"></div>
-                </div>
-                <div className="bg-w95-grey h-4 w-4 flex items-center p-0.5">
-                    <Image 
-                        src="/images/x_icon.svg" 
-                        alt="X" 
+                </button>
+                <button className="bg-w95-grey hover:bg-w95-dark-grey h-4 w-4 flex items-center p-0.5" onClick={props.onclose ? props.onclose : () => setClosed(true)}>
+                    <Image
+                        src="/images/x_icon.svg"
+                        alt="X"
                         width={16}
                         height={16}
                     />
-                </div>
+                </button>
             </div>
-            <div className="bg-white">
-                {children}
+            <div className={"bg-white" + ((minimized || closed) ? " hidden" : "")}>
+                {props.children}
             </div>
         </div>
     )
