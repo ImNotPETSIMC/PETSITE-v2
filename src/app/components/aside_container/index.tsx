6f5fdb08ed977589def964f82f105ca746aa2a5a
Enzo <enzowederma@gmail.com>
2024-01-02 00:31:19 -0300

    ✨ feat: create AsideContainer component

---
+++ src/app/components/aside_container/index.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image"
+import Link from "next/link"
+
+export const AsideContainer = () => {
+    return (
+        <div className='font-ps2p flex lg:flex-col flex-row gap-4 text-[0.5rem] items-center w-fit h-fit p-2'>
+            <div className='flex flex-col items-center text-center border-white border border-solid bg-white/20 h-fit w-fit'>
+                <Link className='relative h-20 w-20 p-2' href='https://www.youtube.com/@petsimc3135'>
+                    <Image
+                        src="/images/youtube_logo.png"
+                        fill
+                        alt="Youtube's Logo"
+                        style={{padding: "1rem"}}
+                    />
+                </Link>
+                <div className='bg-white w-full px-2'>Youtube</div>
+            </div>
+
+            <div className='flex flex-col items-center text-center border-white border border-solid bg-white/20 h-fit w-fit'>
+                <Link className='relative h-20 w-20 p-2' href='https://open.spotify.com/show/390mvapHgaoTZFQoeusC7z?si=92010368c3744da7'>
+                    <Image
+                        src="/images/spotify_logo.png"
+                        fill
+                        alt="Spotify's Logo"
+                        style={{padding: "1rem"}}
+                    />
+                </Link>
+                <div className='bg-white w-full px-2'>Spotify</div>
+            </div>
+
+            <div className='flex flex-col items-center text-center border-white border border-solid bg-white/20 h-fit w-fit'>
+                <Link className='relative h-20 w-20 p-2' href='https://www.instagram.com/petsimc/'>
+                    <Image
+                        src="/images/instagram_logo.png"
+                        fill
+                        alt="Instagram's Logo"
+                        style={{padding: "1rem"}}
+                    />
+                </Link>
+                <div className='bg-white w-full px-2'>Instagram</div>
+            </div>
+
+            <div className='flex flex-col items-center text-center border-white border border-solid bg-white/20 h-fit w-fit'>
+                <Link className='relative h-20 w-20 p-2' href='#'>
+                    <Image
+                        src="/images/petcast_logo.png"
+                        fill
+                        alt="PETCast's Logo"
+                        className='dark:drop-shadow-[2px_4px_6px_white]'
+                        style={{padding: "1rem"}}
+                    />
+                </Link>
+                <div className='bg-white w-full px-2'>PETCast</div>
+            </div>
+        </div>
+    )
+}
