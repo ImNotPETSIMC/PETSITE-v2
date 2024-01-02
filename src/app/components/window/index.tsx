cf791287bad26ac6fa04f801b288d5e11d7e1556
Enzo <enzowederma@gmail.com>
2024-01-02 00:27:58 -0300

    🌈 style: remove inside border in window

--- src/app/components/window/index.tsx
+++ src/app/components/window/index.tsx
@@ -12,7 +12,7 @@ export const Window = (props: WindowProps) => {
 
     return (
         <div className={"border-w95-grey border-8 border-solid w-fit h-fit transition-all duration-500 " + (closed ? "opacity-0 -z-50" : "")}>
-            <div className="border-w95-grey border-b-2 border-solid w-full h-6 bg-w95-blue flex items-center justify-end gap-2 pl-10 pr-1">
+            <div className="border-w95-grey border-solid w-full h-6 bg-w95-blue flex items-center justify-end gap-2 pl-10 pr-1">
                 <button className="flex items-center bg-w95-grey hover:bg-w95-dark-grey h-fit w-fit p-0.5" onClick={() => setMinimized(true)}>
                     <div className="border-b border-solid border-black h-3 w-3"></div>
                 </button>
